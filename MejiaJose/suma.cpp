1634654af19c21aee5124f33754df00fd7f85a03
josemm0505 <mejiamicoltalol@gmail.com>
2022-03-12 12:54:12 -0500

    Segunda version de suma.cpp

--- MejiaJose/suma.cpp
+++ MejiaJose/suma.cpp
@@ -3,8 +3,10 @@ using namespace std;
 int main()
 {
 	float A,B,C;
+	cout<<"Ingrese dos numeros separados por espacio A B :";
 	cin>>A>>B;
 	C=A+B;
-	cout<<C;
+	cout<<"El resultado es :";
+	cout<<C<<endl;
 	return 0;
 }
